16b23b2c669a190d1548ccde1b07dddfd8a20ad7
toxi693 <80880261+toxi693@users.noreply.github.com>
2021-04-07 19:01:27 +0200

    Create simple_bruteforce.py

---
+++ simple_bruteforce.py
@@ -0,0 +1,30 @@
+letters = 'abcdefghijklmnopqrstuwxyz'
+upper_Letters = letters.upper()
+digits = '0123456789'
+symbols = '!@#$%^&*()'
+
+alphabet_hard =digits + letters + upper_Letters + symbols
+
+alphabet_simple = '0123456789abcdefghijklmnopqrstuwxyz'
+
+#алгоритм основан на системах счисления
+
+
+base = len(alphabet_simple)  #база системы счисления
+
+counter = 0  #счётчик итераций в десятиричной системе исчесления
+
+for counter in range(1000):
+
+    password = ''
+    while counter > 0:
+        counter // base
+        rest = counter % base #остаток от деления, который и является фрагментами числа в нужной системе
+        counter = counter // base
+
+        password = alphabet_simple[rest] + password
+
+    print(password)
+    #алгоритм не выдаёт числа 0, 01,02 ...Только с 1
+
+
