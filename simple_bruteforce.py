b72a0de4069b84a05ee5f664e0069f959fd18d8e
toxi <xseven693@gmail.com>
2021-04-08 09:49:38 +0300

    working bruteForce.py 1.0

--- simple_bruteforce.py
+++ simple_bruteforce.py
@@ -9,22 +9,34 @@ alphabet_simple = '0123456789abcdefghijklmnopqrstuwxyz'
 
 #алгоритм основан на системах счисления
 
-
-base = len(alphabet_simple)  #база системы счисления
-
+alphabet = alphabet_hard
+base = len(alphabet)  #база системы счисления
+length = 0
 counter = 0  #счётчик итераций в десятиричной системе исчесления
 
-for counter in range(1000):
+while True:
 
     password = ''
-    while counter > 0:
-        counter // base
-        rest = counter % base #остаток от деления, который и является фрагментами числа в нужной системе
-        counter = counter // base
+    temp = counter
+    while temp > 0:
+        temp // base
+        rest = temp % base #остаток от деления, который и является фрагментами числа в нужной системе
+        temp = temp // base
 
-        password = alphabet_simple[rest] + password
+        password = alphabet[rest] + password
 
+    while len(password) < length:  #чтобы были цифры с нуля
+        password = '0' + password
+
+    #код с использованием подобранного пороля
+        
     print(password)
-    #алгоритм не выдаёт числа 0, 01,02 ...Только с 1
+
+    if password == alphabet[-1] * length:  #если перебран последний пароль на этой длине
+        length += 1  #переходим к паролям большей длинны
+        counter = 0  #обнуляем счётчик, чтобы снова рисовались нули
+    else:
+        counter += 1
+  
 
 
